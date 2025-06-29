15c2f4dc28e96ecc6b5031b442797ac929ba9d4f
Weslley H V Ferraz <weslleyhenrique800@gmail.com>
2025-06-29 14:28:34 -0400

    w

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,9 +1,15 @@
 import axios from "axios";
 
+const username = 'voltocado';
+const password = 'voltocado';
+
+const authHeader = 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64');
+
 export const api = axios.create({
   baseURL: "http://192.168.11.235:8080/api", // URL base do servidor
     timeout: 10000, // Tempo limite de 10 segundos
-    headers: {
+  headers: {
+        Authorization: authHeader,
         "Content-Type": "application/json",
         "Accept": "application/json"
     }
