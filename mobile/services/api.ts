23eb4c6f9acb24d284bd88bd56a8a5fb0458141d
Weslley H V Ferraz <weslleyhenrique800@gmail.com>
2025-06-29 14:10:25 -0400

    a

--- mobile/services/api.ts
+++ mobile/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.1:8080/api", // URL base do servidor
+  baseURL: "http://192.168.11.235:8080/api", // URL base do servidor
     timeout: 10000, // Tempo limite de 10 segundos
     headers: {
         "Content-Type": "application/json",
