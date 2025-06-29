d794756245e73c72780bf58db7fe3a468a112653
Weslley H V Ferraz <weslleyhenrique800@gmail.com>
2025-06-29 14:37:30 -0400

    w

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
+import { encode } from 'base-64';
 
 const username = 'voltocado';
 const password = 'voltocado';
 
-const authHeader = 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64');
+//const authHeader = 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64');
+const authHeader = 'Basic ' + encode(`${username}:${password}`);
 
 export const api = axios.create({
   baseURL: "http://192.168.11.235:8080/api", // URL base do servidor
