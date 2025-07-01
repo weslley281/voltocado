4013b4f790eea84d335b9a1c14a6890f74795c1c
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2025-07-01 16:25:51 -0400

    a

--- backend/src/main/java/com/voltocado/voltocado/controller/SeriesController.java
+++ backend/src/main/java/com/voltocado/voltocado/controller/SeriesController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/serie")
 public class SeriesController {
-
     private final SeriesService seriesService;
 
     public SeriesController(SeriesService seriesService) {
